f5233f2639e37def1cd4a768d3ee8c73e29fb3b3
apz <antpza@gmail.com>
2022-09-09 02:22:32 +0200

    refactor: replacing spring-boot-maven-plugin with maven-assembly-plugin

--- src/main/java/com/apz/Application.java
+++ src/main/java/com/apz/Application.java
@@ -1,28 +1,30 @@
 package com.apz;
 
 import oracle.jdbc.OracleConnection;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
-@SpringBootApplication
 public class Application {
 
-    final static Logger LOG = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) throws ClassNotFoundException {
 
         for (int i = 0; i < args.length; i++) {
-            LOG.info("arg {} = {}", i, args[i]);
+            logger.info("arg {} = {}", i, args[i]);
         }
 
         Class.forName("oracle.jdbc.driver.OracleDriver");
 
         if (args.length != 3) {
-            LOG.error("Invalid number of arguments: Must provide 3 arguments in the format: <schema_name> <schema_password> jdbc:oracle:thin:@//<host>:<port>/<SID>");
+            logger.error("Invalid number of arguments: Must provide 3 arguments in the format: <schema_name> <schema_password> jdbc:oracle:thin:@//<host>:<port>/<SID>");
             return;
         }
 
@@ -32,25 +34,25 @@ public class Application {
         properties.setProperty(OracleConnection.CONNECTION_PROPERTY_THIN_NET_CONNECT_TIMEOUT, "10000");
 
         try {
-            LOG.info("****** Starting JDBC Connection test *******");
+            logger.info("****** Starting JDBC Connection test *******");
             String sqlQuery = "select sysdate from dual";
 
             Connection conn = DriverManager.getConnection(args[2], properties);
             conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            LOG.info("Running SQL query: [{}]", sqlQuery);
+            logger.info("Running SQL query: [{}]", sqlQuery);
             ResultSet resultSet = statement.executeQuery(sqlQuery);
 
             while (resultSet.next()) {
-                LOG.info("Result of SQL query: [{}]", resultSet.getString(1));
+                logger.info("Result of SQL query: [{}]", resultSet.getString(1));
             }
 
             statement.close();
             conn.close();
 
-            LOG.info("JDBC connection test successful!");
+            logger.info("JDBC connection test successful!");
         } catch (SQLException ex) {
-            LOG.error("Exception occurred connecting to database: {}", ex.getMessage());
+            logger.error("Exception occurred connecting to database: {}", ex.getMessage());
         }
     }
 }
