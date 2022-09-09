f489f631f0a119c1385202f4df98a074ce89934a
apz <antpza@gmail.com>
2022-09-09 02:38:53 +0200

    refactor: connection attempt

--- src/main/java/com/apz/Application.java
+++ src/main/java/com/apz/Application.java
@@ -16,6 +16,7 @@ import static oracle.jdbc.OracleConnection.CONNECTION_PROPERTY_THIN_NET_CONNECT_
 public class Application {
 
     private static final Logger logger = LogManager.getLogger(Application.class);
+    private static final String SELECT_SYSDATE_FROM_DUAL = "select sysdate from dual";
 
     public static void main(String[] args) {
 
@@ -27,29 +28,10 @@ public class Application {
             logger.error("Invalid number of arguments: Must provide 3 arguments in the format: <schema_name> <schema_password> jdbc:oracle:thin:@//<host>:<port>/<SID>");
             return;
         }
+
         final OracleDataSource oracleDataSource = oracleDataSource(args[0], args[1], args[2], 10000);
 
-        try {
-            logger.info("****** Starting JDBC Connection test *******");
-            String sqlQuery = "select sysdate from dual";
-
-            Connection conn = oracleDataSource.getConnection();
-            conn.setAutoCommit(false);
-            Statement statement = conn.createStatement();
-            logger.info("Running SQL query: [{}]", sqlQuery);
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-            while (resultSet.next()) {
-                logger.info("Result of SQL query: [{}]", resultSet.getString(1));
-            }
-
-            statement.close();
-            conn.close();
-
-            logger.info("JDBC connection test successful!");
-        } catch (SQLException ex) {
-            logger.error("Exception occurred connecting to database: {}", ex.getMessage());
-        }
+        makeConnectionAttempt(oracleDataSource);
     }
 
     private static OracleDataSource oracleDataSource(final String user, final String password, final String url, final int transportConnectTimeout) {
@@ -69,6 +51,24 @@ public class Application {
             logger.error("Error creating oracle data source: {}", e.getMessage());
             System.exit(1);
             return null;
+        }
+    }
+
+    private static void makeConnectionAttempt(final OracleDataSource oracleDataSource) {
+        logger.debug("Running SQL query: [{}]", SELECT_SYSDATE_FROM_DUAL);
+
+        try (final Connection connection = oracleDataSource.getConnection();
+             final Statement statement = connection.createStatement();
+             final ResultSet resultSet = statement.executeQuery(SELECT_SYSDATE_FROM_DUAL)) {
+
+            while (resultSet.next()) {
+                logger.debug("Result of SQL query: [{}]", resultSet.getString(1));
+            }
+
+            logger.info("JDBC connection test successful!");
+
+        } catch (final SQLException e) {
+            logger.warn("Exception occurred connecting to {}: {}", "oracleDataSource.getURL()", e.getMessage());
         }
     }
 }
